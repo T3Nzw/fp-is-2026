3ac71e350202795c0961e7ab50b7ee0caecdafae
T3Nzw <t3nszw@gmail.com>
2026-03-23 08:10:50 +0200

    solutions on list problems

--- 03. Lists/Solutions.hs
+++ 03. Lists/Solutions.hs
@@ -1,6 +1,22 @@
 module Solutions where
 
-import Prelude hiding (drop, head, init, last, product, reverse, splitAt, tail, take, zip)
+-- много от задачите имат по-хубави решения
+-- с функция от по-висок ред :)
+
+import Prelude hiding
+  ( drop
+  , head
+  , init
+  , last
+  , product
+  , repeat
+  , replicate
+  , reverse
+  , splitAt
+  , tail
+  , take
+  , zip
+  )
 
 -- [1,2,3]
 -- 1 : (2 : (3 : []))
@@ -143,3 +159,189 @@ mergeSorted l1 [] = l1
 mergeSorted l1@(x : xs) l2@(y : ys)
   | x < y = x : mergeSorted xs l2
   | otherwise = y : mergeSorted l1 ys
+
+pack :: Eq a => [a] -> [[a]]
+pack lst = go [] lst
+ where
+  go :: Eq a => [a] -> [a] -> [[a]]
+  go acc [] = [acc]
+  go acc [x] = [x : acc]
+  go acc (x : xs@(y : _))
+    | x == y = go (x : acc) xs
+    | otherwise = (x : acc) : go [] xs
+
+-- >>> pack "aabbbccda"
+-- ["aa","bbb","cc","d","a"]
+
+rle :: Eq a => [a] -> [(Int, a)]
+rle lst = go (pack lst)
+ where
+  go :: Eq a => [[a]] -> [(Int, a)]
+  go [] = []
+  go (x : xs) = (length x, head x) : go xs
+
+-- >>> rle "aabbbccda"
+-- [(2,'a'),(3,'b'),(2,'c'),(1,'d'),(1,'a')]
+
+replicate :: Int -> a -> [a]
+replicate n x
+  | n <= 0 = []
+  | otherwise = x : replicate (n - 1) x
+
+-- >>> replicate 4 'a'
+-- "aaaa"
+
+decodeRle :: [(Int, a)] -> [a]
+decodeRle [] = []
+decodeRle ((n, x) : xs) = replicate n x ++ decodeRle xs
+
+-- >>> decodeRle [(2,'a'),(3,'b'),(2,'c'),(1,'d'),(1,'a')]
+-- "aabbbccda"
+
+cartesian :: [a] -> [b] -> [(a, b)]
+cartesian [] _ = []
+cartesian (x : xs) ys = x `pairWithEvery` ys ++ cartesian xs ys
+ where
+  pairWithEvery :: a -> [b] -> [(a, b)]
+  pairWithEvery _ [] = []
+  pairWithEvery x (y : ys) = (x, y) : pairWithEvery x ys
+
+-- >>> cartesian [1,2,3] "ab"
+-- [(1,'a'),(1,'b'),(2,'a'),(2,'b'),(3,'a'),(3,'b')]
+
+cartesian' :: [a] -> [b] -> [(a, b)]
+cartesian' xs ys = [(x, y) | x <- xs, y <- ys]
+
+-- >>> cartesian' [1,2,3] "ab"
+-- [(1,'a'),(1,'b'),(2,'a'),(2,'b'),(3,'a'),(3,'b')]
+
+subsets :: [a] -> [[a]]
+subsets [] = [[]]
+subsets (x : xs) = let rest = subsets xs in addToEach x rest ++ rest
+ where
+  addToEach :: a -> [[a]] -> [[a]]
+  addToEach _ [] = []
+  addToEach x (xs : xss) = (x : xs) : addToEach x xss
+
+-- >>> subsets [1,2,3]
+-- [[1,2,3],[1,2],[1,3],[1],[2,3],[2],[3],[]]
+
+combinations :: Int -> [a] -> [[a]]
+combinations k lst = filterByLength k (subsets lst)
+ where
+  filterByLength :: Int -> [[a]] -> [[a]]
+  filterByLength _ [] = []
+  filterByLength k (xs : xss)
+    | length xs == k = xs : filterByLength k xss
+    | otherwise = filterByLength k xss
+
+-- >>> combinations 2 [1,2,3]
+-- [[1,2],[1,3],[2,3]]
+
+isPrefixOf :: Eq a => [a] -> [a] -> Bool
+[] `isPrefixOf` _ = True
+_ `isPrefixOf` [] = False
+(x : xs) `isPrefixOf` (y : ys) = x == y && xs `isPrefixOf` ys
+
+-- >>> [1,2] `isPrefixOf` [1,2,3]
+-- True
+
+-- >>> "ab" `isPrefixOf` "ab"
+-- True
+
+isSuffixOf :: Eq a => [a] -> [a] -> Bool
+suf `isSuffixOf` lst = reverse suf `isPrefixOf` reverse lst
+
+-- >>> "ab" `isSuffixOf` "ab"
+-- True
+
+-- >>> "ab" `isSuffixOf` "abc"
+-- False
+-- списък е инфиксен на друг тстк
+-- е префикс на някой суфикс на списъка
+isInfixOf :: Eq a => [a] -> [a] -> Bool
+inf `isInfixOf` [] = null inf
+inf `isInfixOf` lst@(_ : xs) =
+  inf `isPrefixOf` lst || inf `isInfixOf` xs
+
+-- >>> "ab" `isInfixOf` "abc"
+-- True
+
+-- >>> "bc" `isInfixOf` "abc"
+-- True
+
+-- >>> "bc" `isInfixOf` "abcd"
+-- True
+
+-- тази ще я оставим така, защото иначе би изглеждала
+-- прекалено мизерна (т.е. без функции от по-висок ред :) )
+permutations :: Eq a => [a] -> [[a]]
+permutations [] = [[]]
+permutations lst = concat [map (x :) res | x <- lst, let res = permutations $ filter (/= x) lst]
+
+-- това е неоптимално спрямо паметта :)
+repeat :: a -> [a]
+repeat x = x : repeat x
+
+-- >>> take 10 (repeat 6)
+-- [6,6,6,6,6,6,6,6,6,6]
+
+-- константна памет
+repeat' :: a -> [a]
+repeat' x = let y = x : y in y
+
+-- >>> take 10 (repeat' 6)
+-- [6,6,6,6,6,6,6,6,6,6]
+
+nats :: [Int]
+nats = go 0
+ where
+  go i = i : go (i + 1)
+
+-- >>> take 10 nats
+-- [0,1,2,3,4,5,6,7,8,9]
+
+powersOf2 :: [Int]
+powersOf2 = go 1
+ where
+  go :: Int -> [Int]
+  go i = i : go (i * 2)
+
+-- >>> take 10 powersOf2
+-- [1,2,4,8,16,32,64,128,256,512]
+
+isPrime :: Int -> Bool
+isPrime n = hasNoDivisorsIn n [2 .. n - 1]
+ where
+  hasNoDivisorsIn :: Int -> [Int] -> Bool
+  hasNoDivisorsIn _ [] = True
+  hasNoDivisorsIn k (x : xs) = k `mod` x /= 0 && hasNoDivisorsIn k xs
+
+-- >>> isPrime 2
+-- True
+
+-- >>> isPrime 3
+-- True
+
+-- >>> isPrime 4
+-- False
+
+primes :: [Int]
+primes = go 2
+ where
+  go :: Int -> [Int]
+  go i
+    | isPrime i = i : go (i + 1)
+    | otherwise = go (i + 1)
+
+-- >>> take 10 primes
+-- [2,3,5,7,11,13,17,19,23,29]
+
+fibs :: [Int]
+fibs = go 0 1
+ where
+  go :: Int -> Int -> [Int]
+  go prev cur = prev : go cur (prev + cur)
+
+-- >>> take 10 fibs
+-- [0,1,1,2,3,5,8,13,21,34]
